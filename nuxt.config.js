a8bd47c5a35fbd9f613bb5938a941cd0e74c694f
amirpeace1080 <amirpeace1080@gmail.com>
2021-07-13 22:08:21 +0430

    plugin custom

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    './plugin/myPlugin.client.js'
+    './plugin/myPlugin.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ pages/plugin/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>Plugin</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    fetch(){
+        console.log("fetch");
+        this.$myTestPlugin.test()
+    }
+}
+</script>
+
+<style>
+
+</style>
