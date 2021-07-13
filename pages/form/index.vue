729e50c7ad2ec4132d631a5e58230829edd8d60b
amirpeace1080 <amirpeace1080@gmail.com>
2021-07-13 19:55:52 +0430

    form in v-model

---
+++ pages/form/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Form</h1>
+    <label>First Name : </label>
+    <input type="text" v-model="firstName" />
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            firstName: '',
+        }
+    },
+    watch:{
+        firstName(value){
+            console.log("first name is : ", value);
+        }
+    }
+}
+</script>
+
+<style></style>
