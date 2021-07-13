740a2c8dbffa9d8c448ffa73e9c23b85dfe411df
amirpeace1080 <amirpeace1080@gmail.com>
2021-07-13 21:02:49 +0430

    custom number picker

---
+++ components/ui/AppNumberPicker.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <h1>Number Picker</h1>
+
+      <button @click="minus">Minus</button>
+      {{ value }}
+      <button @click="plus">Plus</button>
+  </div>
+</template>
+
+<script>
+export default {
+    model:{
+        prop: 'value',
+        event: 'change'
+    },
+    props:{
+        value:{
+            type: Number,
+            required: true,
+        },
+        minValue:{
+            type: Number,
+            default: 0,
+        },
+        plusValue:{
+            type: Number,
+            default: 10
+        }
+    },
+    methods: {
+        plus(){
+            if(this.value+1 <= this.plusValue){
+                this.$emit("change", this.value+1)
+            }
+        },
+        minus(){
+            if(this.value-1 >= this.minValue){
+                this.$emit("change", this.value-1)
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- pages/form/index.vue
+++ pages/form/index.vue
@@ -3,19 +3,23 @@
     <h1>Form</h1>
     <label>First Name : </label>
     <AppTextInput v-model="firstName" />
+    <AppNumberPicker :minValue="4" v-model="count" />
   </div>
 </template>
 
 <script>
 import AppTextInput from '@/components/ui/AppTextInput.vue'
+import AppNumberPicker from '@/components/ui/AppNumberPicker.vue'
 
 export default {
     components:{
         AppTextInput,
+        AppNumberPicker,
     },
     data() {
         return {
             firstName: '',
+            count: 1,
         }
     },
     watch:{
